1a7791885e93535f630d66fbd5d2eed07b3b80fb
Tutmic Sedina <147973208+sedinatutmic@users.noreply.github.com>
2024-06-02 15:50:50 +0200

    Update Blog.jsx

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,6 +3,11 @@ import './blog.css'
 
 import Aos from 'aos'
 import 'aos/dist/aos.css'
+import { BsArrowUpShort } from 'react-icons/bs'
+import img from '../../assets/viber_image_2024-05-21_13-27-24-989.jpg'
+import img2 from '../../assets/viber_image_2024-05-21_13-27-24-989.jpg'
+import img3 from '../../assets/viber_image_2024-05-21_13-27-24-989.jpg'
+import img4 from '../../assets/viber_image_2024-05-21_13-27-23-859.jpg'
 
 const Blog = () => {
    useEffect(()=>{
@@ -12,6 +17,84 @@ const Blog = () => {
     <div>Blog</div>
   )
 }
+const Post=[
+{
+  id:1,
+  postImg: img,
+  title: 'Mostar',
+  desc:'Mostar'
+},
+{
+  id:2,
+  postImg: img2,
+  title: 'Mostar',
+  desc:'Mostar'
+},
+{
+  id:3,
+  postImg: img3,
+  title: 'Mostar',
+  desc:'Mostar'
+},
+{
+  id:4,
+  postImg: img4,
+  title: 'Mostar',
+  desc:'Mostar'
+}
+
+
+]
+
+const Blog = () => {
+  return (
+    <section className='blogContainer section'>
+      <div className="secConatiner">
+        <div className="secIntro">
+          <h2 className='secTitle'>
+            Lorem ipsum dolor sit amet.
+          </h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Voluptates fugit veritatis porro soluta assumenda rem.</p>
+        </div>
+        <div className="mainContainer grid">
+
+          {
+            Posts.map( ({id, postImg, title, desc}) =>{
+              return (
+                <div className="singlePost grid">
+                <div className="imgDiv">
+                  <img src={postImg}alt={title} />
+                </div>
+                <div className="postDetails">
+                  <h3>
+                    {title}
+                  </h3>
+                  <p>{desc}</p>
+                </div>
+                <a href="" className="flex">
+                  <bsArrowrightshort className="icon">
+                    Read more
+                  </bsArrowrightshort>
+    
+                 
+                </a>
+              </div>
+
+              )
+            }
+          )
+          }
+         
+
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default Blog
 /* u div h2 secTitle dodati: data-aos ="fade-up " data-aos-duration="2000"
 
 i u <p> dodati: data-aos ="fade-up " data-aos-duration="2500"
